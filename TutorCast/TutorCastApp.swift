1ecdf9248945790e339da299e106d3bd220fad36
Yankey <yankey.emma@yahoo.com>
2026-03-15 18:48:20 +0300

    Initial Commit

---
+++ TutorCast/TutorCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorCastApp.swift
+//  TutorCast
+//
+//  Created by Nana on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TutorCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
